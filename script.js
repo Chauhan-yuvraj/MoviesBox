c160d8014e9c66cd4bee4315b4ff648e1366edb8
Chauhan-yuvraj <yuvibannna@gmail.com>
2024-03-10 18:29:25 +0530

    fronted done

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     var loader = document.getElementById('preloader');
+//     if (loader) {
+//         loader.style.display = 'none';
+//     }
+// });
