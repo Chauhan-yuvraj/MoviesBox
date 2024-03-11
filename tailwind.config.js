caeff6346edb873dcebdc0c335dd77bff55c5a12
Chauhan-yuvraj <yuvibannna@gmail.com>
2024-03-11 17:03:16 +0530

    made new pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html" , "./pages/SignUp.html" ,  "./pages/Login.html" , "./pages/MovieDetails.html" , "./pages/cart.html" ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
