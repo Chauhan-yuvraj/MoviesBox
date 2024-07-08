14e8d5a3ee3ccadd2bb04de5670a055d18d739f8
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-08 18:00:58 +0530

    Update repository.js

--- repository.js
+++ repository.js
@@ -3,7 +3,7 @@
   "description": "Movie Box: A web app for downloading international movies, offering a vast library, high-quality downloads, user reviews, and easy navigation for an enhanced viewing experience.",
   "year": "2023",
   "role": "front-end developer",
-  "avatar": "movieBox.png",
+  "avatar": "https://www.pngall.com/wp-content/uploads/13/Movie-PNG-Free-Image.png",
   "demo": "https://chauhan-yuvraj.github.io/MoviesBox/",
   "github": "https://github.com/Chauhan-yuvraj/MoviesBox"
 }
