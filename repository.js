462685ef9270fb6c9a355c53640efd30b28a4198
CHAUHAN YUVRAJ SINGH <113570060+Chauhan-yuvraj@users.noreply.github.com>
2024-07-08 17:49:50 +0530

    Create repository.js

---
+++ repository.js
@@ -0,0 +1,9 @@
+{
+  "name": "MovieBox : The single Location of all your favourite Movies",
+  "description": "Movie Box: A web app for downloading international movies, offering a vast library, high-quality downloads, user reviews, and easy navigation for an enhanced viewing experience.",
+  "year": "2023",
+  "role": "front-end developer",
+  "avatar": "movieBox.png",
+  "demo": "https://chauhan-yuvraj.github.io/MoviesBox/",
+  "github": "https://github.com/Chauhan-yuvraj/MoviesBox"
+}
